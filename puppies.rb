5a8db26a20f1f9b9ad4fd5d04723ac048d192ad0
Kelly Cotter <kjc853@gmail.com>
2018-07-11 13:38:00 +0000

    Done.

--- puppies.rb
+++ puppies.rb
@@ -2,13 +2,55 @@
 
 
 
+def no_puppies(number_of_puppies)
+  if number_of_puppies > 0
+     "No more puppies!"
+  else number_of_puppies  
+     "Good job, Rachel !"
+ end
+end
+
+ puts no_puppies(4)
+
+
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
 
+def less_puppies(number_of_puppies)
+  if number_of_puppies  < 3
+     "Good job, Rachel!"
+  elsif number of puppies == 3
+     "Slow down, Rachel"
+  else   
+     "Get back to your animal restrictions, Rachel!"
+ end 
+end
 
-
+puts less_puppies(10)
 # 3. We've decided to give Rachel a little more control over her puppy obsession. Write a method called some_puppies that takes in two arguments - the number of puppies she has found and a maximum puppy allowance. This method should tell Rachel good job if she gets less than half her maximum allowance and to get back on her puppy restriction when she exceeds her maximum.
 
+def some_puppies(number_of_puppies , maximum_puppy_allowance)
+  if maximum_puppy_allowance / 2 > number_of_puppies
+    puts "Good job, Rachel!"
+  else  
+    puts "Get back to your animal goals!"
+  end
+end 
 
-
+puts some_puppies(15,33)
 # BONUS
 # 4. Rachel has decided to track both her puppy allowance AND her cat allowance. Write a method called new_animal accumulation that takes in two arguments - number of puppies and number of cats. As long as she gets zero puppies OR zero cats this method should return "Good job!". If she gets 1 or more puppies AND 1 or more cats it should tell her she should really slow back down!
+
+
+def both_animals(puppies, cats)
+  if puppies == 0 || cats == 0
+    "Good job!"
+  elsif puppies > 0 && cats > 0 
+    "Get back to your animal goals!"
+  end
+end
+
+puts new_animal(5,3)
+
+
+
+
